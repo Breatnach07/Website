1a5ba7e507ac5d7d111dc63abf9b0e233cdf3094
Breatnach07 <breatnach07@gmail.com>
2026-06-29 13:59:17 +0100

    Private Notes

--- functions/api/get-notes.js
+++ functions/api/get-notes.js
@@ -1,12 +1,9 @@
 export async function onRequestGet(context) {
   const scriptURL = context.env.GOOGLE_SHEETS_SCRIPT_URL;
-
-  // Append a query parameter to tell Google you want to read, not write
-  const response = await fetch(`${scriptURL}?action=getRecent`, {
-    method: "GET"
-  });
-  
+  // We add a 'getRecent' parameter so your Google Script knows what to do
+  const response = await fetch(`${scriptURL}?action=getRecent`);
   const data = await response.json();
+  
   return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" }
   });
